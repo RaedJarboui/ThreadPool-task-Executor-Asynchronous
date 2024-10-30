85e8a94f7fce101d09652b0d5bb4c9b53d4091b6
RaedJarboui <raedjarboui1998@gmail.com>
2024-10-30 16:17:35 +0100

    add @Async for services methods to be executed asynchronously

--- src/main/java/com/spring/boot/async/service/OrderFulfillmentService.java
+++ src/main/java/com/spring/boot/async/service/OrderFulfillmentService.java
@@ -3,6 +3,7 @@ package com.spring.boot.async.service;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.spring.boot.async.model.Order;
@@ -29,26 +30,31 @@ public class OrderFulfillmentService {
 		return order;
 	}
 
+	@Async("asyncTaskExecutor")
 	public void notifyUser(Order order) throws InterruptedException {
 		Thread.sleep(4000L);
 		log.info("Notified to the user " + Thread.currentThread().getName());
 	}
 
+	@Async("asyncTaskExecutor")
 	public void assignVendor(Order order) throws InterruptedException {
 		Thread.sleep(5000L);
 		log.info("Assign order to vendor " + Thread.currentThread().getName());
 	}
 
+	@Async("asyncTaskExecutor")
 	public void packaging(Order order) throws InterruptedException {
 		Thread.sleep(2000L);
 		log.info("Order packaging completed " + Thread.currentThread().getName());
 	}
 
+	@Async("asyncTaskExecutor")
 	public void assignDeliveryPartner(Order order) throws InterruptedException {
 		Thread.sleep(10000L);
 		log.info("Delivery partner assigned " + Thread.currentThread().getName());
 	}
 
+	@Async("asyncTaskExecutor")
 	public void assignTrailerAndDispatch(Order order) throws InterruptedException {
 		Thread.sleep(3000L);
 		log.info("Trailer assigned and Order dispatched " + Thread.currentThread().getName());
